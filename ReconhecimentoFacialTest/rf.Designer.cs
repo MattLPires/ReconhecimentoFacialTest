69f5e526a91fd1ae186d6ad54f8bd939ac7a2e80
Matt <Matt@MATHEUS>
2022-05-25 19:45:26 -0300

    Adicionar arquivos de projeto.

---
+++ ReconhecimentoFacialTest/rf.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace ReconhecimentoFacialTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.camBox = new Emgu.CV.UI.ImageBox();
+            this.btnDetec = new System.Windows.Forms.Button();
+            this.btnCad = new System.Windows.Forms.Button();
+            this.txtCad = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.camBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // camBox
+            // 
+            this.camBox.Location = new System.Drawing.Point(25, 24);
+            this.camBox.Name = "camBox";
+            this.camBox.Size = new System.Drawing.Size(532, 559);
+            this.camBox.TabIndex = 2;
+            this.camBox.TabStop = false;
+            // 
+            // btnDetec
+            // 
+            this.btnDetec.BackColor = System.Drawing.Color.SeaShell;
+            this.btnDetec.Location = new System.Drawing.Point(699, 140);
+            this.btnDetec.Name = "btnDetec";
+            this.btnDetec.Size = new System.Drawing.Size(244, 96);
+            this.btnDetec.TabIndex = 3;
+            this.btnDetec.Text = "Verificar ";
+            this.btnDetec.UseVisualStyleBackColor = false;
+            this.btnDetec.Click += new System.EventHandler(this.btnDetec_Click);
+            // 
+            // btnCad
+            // 
+            this.btnCad.BackColor = System.Drawing.Color.Honeydew;
+            this.btnCad.Location = new System.Drawing.Point(608, 21);
+            this.btnCad.Name = "btnCad";
+            this.btnCad.Size = new System.Drawing.Size(161, 42);
+            this.btnCad.TabIndex = 4;
+            this.btnCad.Text = "Cadastrar pessoa";
+            this.btnCad.UseVisualStyleBackColor = false;
+            this.btnCad.Click += new System.EventHandler(this.btnCad_Click);
+            // 
+            // txtCad
+            // 
+            this.txtCad.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCad.Location = new System.Drawing.Point(787, 29);
+            this.txtCad.MaximumSize = new System.Drawing.Size(250, 90);
+            this.txtCad.MinimumSize = new System.Drawing.Size(250, 25);
+            this.txtCad.Name = "txtCad";
+            this.txtCad.Size = new System.Drawing.Size(250, 24);
+            this.txtCad.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1080, 603);
+            this.Controls.Add(this.txtCad);
+            this.Controls.Add(this.btnCad);
+            this.Controls.Add(this.btnDetec);
+            this.Controls.Add(this.camBox);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.camBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox camBox;
+        private System.Windows.Forms.Button btnDetec;
+        private System.Windows.Forms.Button btnCad;
+        private System.Windows.Forms.TextBox txtCad;
+    }
+}
+
---
+++ ReconhecimentoFacialTest/rf.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace ReconhecimentoFacialTest
+{
+    public partial class Form1 : Form
+    {
+
+        //Declarando variáveis
+
+        MCvFont font = new MCvFont(Emgu.CV.CvEnum.FONT.CV_FONT_HERSHEY_TRIPLEX, 0.6d, 0.6d);
+        HaarCascade faceDetectada;
+        Image<Bgr, Byte> Frame;
+        Capture cam;
+        Image<Gray, byte> result;
+        Image<Gray, byte> TrainedFace = null;
+        Image<Gray, byte> grayFace = null;
+        List<Image<Gray, byte>> trainingImages = new List<Image<Gray, byte>>();
+        List<string> labels = new List<string>();
+        List<string> Users = new List<string>();
+        int Count, NumLabels, t;
+        string nome, nomes = null;
+
+        private void btnDetec_Click(object sender, EventArgs e)
+        {
+            cam = new Capture();
+            cam.QueryFrame();
+            Application.Idle += new EventHandler(FrameProcedure);
+        }
+
+        private void btnCad_Click(object sender, EventArgs e)
+        {
+            Count = Count + 1;
+            grayFace = cam.QueryGrayFrame().Resize(600, 500, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            MCvAvgComp[][] DetectedFaces = grayFace.DetectHaarCascade(faceDetectada, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(90, 90));
+            foreach (MCvAvgComp f in DetectedFaces[0])
+            {
+                TrainedFace = Frame.Copy(f.rect).Convert<Gray, Byte>();
+                break;
+            }
+            TrainedFace = result.Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            trainingImages.Add(TrainedFace);
+            labels.Add(txtCad.Text);
+            File.WriteAllText(Application.StartupPath + "/Faces/Faces.txt", trainingImages.ToArray().Length.ToString() + ",");
+            for (int i = 1; i<trainingImages.ToArray().Length + 1; i++)
+            {
+                trainingImages.ToArray()[i - 1].Save(Application.StartupPath + "/Faces/face" + i + ".bmp");
+                File.AppendAllText(Application.StartupPath + "/Faces/Faces.txt", labels.ToArray()[i - 1] + ",");
+
+            }
+            MessageBox.Show(txtCad.Text + " Cadastrado com sucesso!");
+        }
+
+        private void FrameProcedure(object sender, EventArgs e)
+        {
+            Users.Add("");
+            Frame = cam.QueryFrame().Resize(600, 500, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+            grayFace = Frame.Convert<Gray, Byte>();
+            MCvAvgComp[][] facesDetectadasNow = grayFace.DetectHaarCascade(faceDetectada, 1.2, 10, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(90, 90));
+            foreach (MCvAvgComp f in facesDetectadasNow[0])
+            {
+                result = Frame.Copy(f.rect).Convert<Gray, Byte>().Resize(100,100,Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+                Frame.Draw(f.rect, new Bgr(Color.Green), 3);
+                if (trainingImages.ToArray().Length !=0)
+                {
+                    MCvTermCriteria termCriterias = new MCvTermCriteria(Count, 0.001);
+                    EigenObjectRecognizer reconhecer = new EigenObjectRecognizer(trainingImages.ToArray(), labels.ToArray(), 1500, ref termCriterias);
+                    nome = reconhecer.Recognize(result);
+                    Frame.Draw(nome, ref font, new Point(f.rect.X - 2, f.rect.Y - 2), new Bgr(Color.Red));
+
+                }
+
+                Users.Add("");
+            }
+            camBox.Image = Frame;
+            nomes = "";
+            Users.Clear();
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Declarando o HaarCascade para a detecção
+            faceDetectada = new HaarCascade("haarcascade_frontalface_default.xml");
+            try
+            {
+                string LabelsInf = File.ReadAllText(Application.StartupPath + "/Faces/Faces.txt");
+                string[] Labels = LabelsInf.Split(',');
+
+                //O primeiro campo vai armazenar o número de faces salvas
+
+                NumLabels = Convert.ToInt16(Labels[0]);
+
+                Count = NumLabels;
+                string FacesLoad;
+                for (int i=1; i<NumLabels + 1; i++)
+                {
+                    FacesLoad = "face" + i + ".bmp";
+                    trainingImages.Add(new Image<Gray, byte>(Application.StartupPath + $"/Faces/{FacesLoad}"));
+                    labels.Add(Labels[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Nenhum dado encontrado");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
